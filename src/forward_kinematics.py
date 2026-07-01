8e9c06a60670b5c07b8f7508ac323ea6402b8f53
ritisha2503 <ritisha2503@gmail.com>
2026-07-01 11:13:20 +0530

    forward kinematics

---
+++ src/forward_kinematics.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def forward_kinematics(theta1, theta2, l1, l2):
+    x1 = l1 * np.cos(theta1)
+    y1 = l1 * np.sin(theta1)
+    x2 = x1 + l2 * np.cos(theta1 + theta2)
+    y2 = y1 + l2 * np.sin(theta1 + theta2)
+    return x1, y1, x2, y2
