582b1a82855b9051967a37554eaa6d7cfcd44666
ritisha2503 <ritisha2503@gmail.com>
2026-07-01 19:11:25 +0530

    tracing a single point

--- src/forward_kinematics.py
+++ src/forward_kinematics.py
@@ -12,11 +12,22 @@ L1 = 100
 L2 = 80
 fig = plt.figure(figsize=(8, 8))
 
-for i in range(360):
+theta1_target = np.radians(100)
+theta2_target = np.radians(40)
+
+theta1_current = 0
+theta2_current = 0
+Kp = 0.5
+dt = 0.02
+tolerance = np.radians(0.5)
+
+for frame in range(300):
+    error1 = theta1_target - theta1_current
+    error2 = theta2_target - theta2_current
+    theta1_current += error1 * Kp * dt
+    theta2_current += error2 * Kp * dt
     plt.cla()
-    theta1 = np.radians(i)
-    theta2 = np.radians(60)
-    x1, y1, x2, y2 = forward_kinematics(theta1, theta2, L1, L2)
+    x1, y1, x2, y2 = forward_kinematics(theta1_current, theta2_current, L1, L2)
     plt.axis("equal")
     plt.xlim(-200,200)
     plt.ylim(-200,200)
@@ -28,3 +39,5 @@ for i in range(360):
     plt.plot([x1, x2], [y1, y2], color = 'blue')
     plt.scatter([0, x1, x2], [0, y1, y2], color='black')
     plt.pause(0.02)
+    if abs(error1) < tolerance and abs(error2) < tolerance:
+        break
